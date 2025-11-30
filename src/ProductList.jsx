6ed19c9609ea4ea85192225f9b919bf306237f9c
Tiffany Parise <tparise@umich.edu>
2025-11-30 11:38:59 -0500

    css updates

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -310,7 +310,7 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-title">{plant.name}</div> {/* Display plant name */}
                                         {/* Display other plant details like description and cost */}
                                         <div className="product-description">{plant.description}</div> {/* Display plant description */}
-                                        <div className="product-cost">{plant.cost}</div> {/* Display plant cost */}
+                                        <div className="product-price">{plant.cost}</div> {/* Display plant cost */}
                                         <button
                                             className={`product-button ${isItemInCart(plant.name) ? "added-to-cart" : ""}`}
                                             onClick={() => handleAddToCart(plant)} // Handle adding plant to cart
